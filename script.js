2b40e706e69d7b3bd14a156478844dd7afe407c9
jimaggie <45106702+jimaggie@users.noreply.github.com>
2019-10-13 04:25:37 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ function FindQuote() {
           'heheh hi dubhacks! :3. (UwU), ˄·͈༝·͈˄₍˄·͈༝·͈˄( ͒ ु•·̫• ू ͒)˄·͈༝·͈˄₎˄·͈༝·͈˄₎'
   ];
 
-  var seachTerms = ['biking is good for health and environment', 'composting food waste',
+  var seachTerms = ['bikingigood for health and environment', 'composting food waste',
   'reusable bags', 'reusable toilet paper', 'washing clothing less', 'weebaboo'];
 
  var random = Math.floor(Math.random() * quotionary.length);
@@ -54,7 +54,7 @@ function FindQuote() {
 
   document.getElementById("quoteID").innerHTML = quote;
   document.getElementById("searchID").onclick = function () {
-    window.open("https://www.google.com/serach?q="+search);
+    window.open("https://www.google.com/search?q="+search);
   }
 }
 
